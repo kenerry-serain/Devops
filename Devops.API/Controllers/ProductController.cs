c53dce34ec63d077645c7d35402938d272ff6a3b
root <kserain.development@gmail.com>
2018-12-09 20:06:55 -0200

    ..

--- Devops.API/Controllers/ProductController.cs
+++ Devops.API/Controllers/ProductController.cs
@@ -11,6 +11,7 @@ namespace Devops.API.Controllers
     {
         private static readonly IList<Product> ProductCollection = new List<Product>();
         
+        
         /// <summary>
         /// Select all products
         /// </summary>
@@ -26,90 +27,90 @@ namespace Devops.API.Controllers
             return NoContent();
         }
         
-        /// <summary>
-        /// Select a single product
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("{id}", Name = "GetById")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
-        public IActionResult GetById([FromRoute]string id)
-        {
-            //Avoiding invalid id to call database
-            if (string.IsNullOrEmpty(id)) 
-                return BadRequest();            
-            
-            var product = ProductCollection.SingleOrDefault(pdt => pdt.Id == id);
-            if (product != null)
-                return Ok(product);
-
-            return NotFound();
-        }
-        
-        /// <summary>
-        /// Creates a single product
-        /// </summary>
-        /// <param name="product"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [ProducesResponseType(201)]
-        public IActionResult Register([FromBody] Product product)
-        {
-            ProductCollection.Add(product);
-            return Created(nameof(GetById), product);
-        }
-
-        /// <summary>
-        /// Updates a single product
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="product"></param>
-        /// <returns></returns>
-        [HttpPut]
-        [Route("{id}")]
-        [ProducesResponseType(202)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public IActionResult Update([FromRoute]string id, [FromBody] Product product)
-        {
-            //Avoiding invalid id to call database
-            if (string.IsNullOrEmpty(id)) 
-                return BadRequest();
-            
-            if (ProductCollection.All(pdt => pdt.Id != id))
-                return NotFound();
-
-            var index = ProductCollection.IndexOf(ProductCollection.SingleOrDefault(pdt => pdt.Id == id));
-            ProductCollection[index] = product.WithId(id);
-            return Accepted(product);
-        }
-        
-                
-        /// <summary>
-        /// Removes a single product
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpDelete]
-        [Route("{id}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public IActionResult Delete([FromRoute]string id)
-        {
-            //Avoiding invalid id to call database
-            if (string.IsNullOrEmpty(id)) 
-                return BadRequest();
-            
-            var product = ProductCollection.SingleOrDefault(pdt => pdt.Id == id);
-            if (product is null) 
-                return NotFound();
-            
-            ProductCollection.Remove(product);
-            return Ok();
-        }
+//        /// <summary>
+//        /// Select a single product
+//        /// </summary>
+//        /// <param name="id"></param>
+//        /// <returns></returns>
+//        [HttpGet]
+//        [Route("{id}", Name = "GetById")]
+//        [ProducesResponseType(200)]
+//        [ProducesResponseType(404)]
+//        [ProducesResponseType(400)]
+//        public IActionResult GetById([FromRoute]string id)
+//        {
+//            //Avoiding invalid id to call database
+//            if (string.IsNullOrEmpty(id)) 
+//                return BadRequest();            
+//            
+//            var product = ProductCollection.SingleOrDefault(pdt => pdt.Id == id);
+//            if (product != null)
+//                return Ok(product);
+//
+//            return NotFound();
+//        }
+//        
+//        /// <summary>
+//        /// Creates a single product
+//        /// </summary>
+//        /// <param name="product"></param>
+//        /// <returns></returns>
+//        [HttpPost]
+//        [ProducesResponseType(201)]
+//        public IActionResult Register([FromBody] Product product)
+//        {
+//            ProductCollection.Add(product);
+//            return Created(nameof(GetById), product);
+//        }
+//
+//        /// <summary>
+//        /// Updates a single product
+//        /// </summary>
+//        /// <param name="id"></param>
+//        /// <param name="product"></param>
+//        /// <returns></returns>
+//        [HttpPut]
+//        [Route("{id}")]
+//        [ProducesResponseType(202)]
+//        [ProducesResponseType(400)]
+//        [ProducesResponseType(404)]
+//        public IActionResult Update([FromRoute]string id, [FromBody] Product product)
+//        {
+//            //Avoiding invalid id to call database
+//            if (string.IsNullOrEmpty(id)) 
+//                return BadRequest();
+//            
+//            if (ProductCollection.All(pdt => pdt.Id != id))
+//                return NotFound();
+//
+//            var index = ProductCollection.IndexOf(ProductCollection.SingleOrDefault(pdt => pdt.Id == id));
+//            ProductCollection[index] = product.WithId(id);
+//            return Accepted(product);
+//        }
+//        
+//                
+//        /// <summary>
+//        /// Removes a single product
+//        /// </summary>
+//        /// <param name="id"></param>
+//        /// <returns></returns>
+//        [HttpDelete]
+//        [Route("{id}")]
+//        [ProducesResponseType(200)]
+//        [ProducesResponseType(400)]
+//        [ProducesResponseType(404)]
+//        public IActionResult Delete([FromRoute]string id)
+//        {
+//            //Avoiding invalid id to call database
+//            if (string.IsNullOrEmpty(id)) 
+//                return BadRequest();
+//            
+//            var product = ProductCollection.SingleOrDefault(pdt => pdt.Id == id);
+//            if (product is null) 
+//                return NotFound();
+//            
+//            ProductCollection.Remove(product);
+//            return Ok();
+//        }
     }
 }
